00b7483f13c34dd437623b2272a7a1e394978dec
RicoLancheros <rico.lancheros@hotmail.com>
2025-01-19 21:14:59 -0500

    Primer Commit, Proyecto Cine!!

---
+++ src/main/java/com/Cine/Project/Movies_Reservation_Rico_Project/MoviesReservationRicoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Cine.Project.Movies_Reservation_Rico_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesReservationRicoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesReservationRicoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cine/Project/Movies_Reservation_Rico_Project/MoviesReservationRicoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cine.Project.Movies_Reservation_Rico_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesReservationRicoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
